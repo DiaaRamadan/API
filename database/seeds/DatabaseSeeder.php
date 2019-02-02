5df6b3ce355f13df3480e49c0331a249241823f0
Diaa <DiaaRamadan>
2019-02-02 22:42:25 +0200

    Relationships created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         //factory(App\Product::class, 50)->create();
-        factory(App\Review::class, 300)->create();
+        //factory(App\Review::class, 300)->create();
     }
 }
